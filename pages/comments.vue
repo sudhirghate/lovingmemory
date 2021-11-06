abf5000faa387916b6c139ada121dbda7ef59f17
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 21:50:00 +0530

    Update comments.vue

--- pages/comments.vue
+++ pages/comments.vue
@@ -7,7 +7,7 @@
       <h1 class="title">Comments</h1>
       <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v12.0" nonce="7euCH3NC"></script>
-<div class="fb-comments" data-href="https://sudhir.magnumad.in" data-width="" data-numposts="100"></div>
+<div class="fb-comments" data-href="https://sudhir.magnumad.in/comments" data-width="" data-numposts="100"></div>
 
     </div>
   </div>
