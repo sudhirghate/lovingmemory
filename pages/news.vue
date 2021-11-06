5c630fde91db204cb2383b973f0596fe298bb34f
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 21:59:05 +0530

    Create news.vue

---
+++ pages/news.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section details">
+      <h1 class="title">News Coverage</h1>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
