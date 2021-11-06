7f37680e9259e8a8bd47cea559ca211978fbe8f8
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 22:09:16 +0530

    Update news.vue

--- pages/news.vue
+++ pages/news.vue
@@ -6,6 +6,16 @@
     <div class="section details">
       <h1 class="title">News Coverage</h1>
 
+<script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
+<script type="text/javascript">
+	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
+		var adobeDCView = new AdobeDC.View({clientId: "ca3a1d6e1d27438c8b1354295f787b98"});
+		adobeDCView.previewFile({
+			content:{location: {url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"}},
+			metaData:{fileName: "Bodea Brochure.pdf"}
+		}, {embedMode: "LIGHT_BOX"});
+	});
+</script>
 
     </div>
   </div>
