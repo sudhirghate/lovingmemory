25df466a413ef52c23dbd8274e3e3d476caa58a6
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 21:14:57 +0530

    Update gallery.vue

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -4,7 +4,7 @@
       <Nav />
     </div>
     <div class="section details">
-      <h1 class="title">Memories</h1>
+      <h1 class="title">Gallery</h1>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/rUp6QcRaAAk?controls=0&modestbranding=1&autohide=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
     </div>
