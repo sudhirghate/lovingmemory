4c9096e110960a461b6108af3296e7198cfb7b48
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 22:37:15 +0530

    Update comments.vue

--- pages/comments.vue
+++ pages/comments.vue
@@ -5,11 +5,7 @@
     </div>
     <div class="section details">
       <h1 class="title">Comments</h1>
-
-
 <iframe src="http://www.facebook.com/plugins/comments.php?href=https://sudhir.cmc.us" scrolling="yes" frameborder="0" style="border:none; overflow:hidden; width:100%; height:3806px;" allowTransparency="true"></iframe>
-
-
 
     </div>
   </div>
