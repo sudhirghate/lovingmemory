1374f3cac0d0693df4169ad813a70db0c7a4cf16
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 21:57:07 +0530

    Update Nav.vue

--- components/Nav.vue
+++ components/Nav.vue
@@ -13,7 +13,7 @@
           </span><span class="menu"><span>Service </span>Livestream</span></NuxtLink></li>
           
         <li><NuxtLink to="/comments"><span class="icon">
-          <svg class="svg-inline--fa fa-comment fa-w-18 fa-fw" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7S4.8 480 8 480c66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32z"></path></svg>
+          <svg class="svg-inline--fa fa-comment fa-w-18 fa-fw" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7S4.8 480 8 480c66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32z"></path></svg>
           </span><span class="menu"><span>Comments </span></span></NuxtLink></li>
 
       </ul>
