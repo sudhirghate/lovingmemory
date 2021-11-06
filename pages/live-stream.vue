0ffd7bae13b8ae3607630f5afef509dcb18ca29c
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 21:11:33 +0530

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -5,18 +5,8 @@
     </div>
     <div class="section details">
       <h1 class="title">Service Livestream</h1>
-      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Church address then at Crematorium address.</p>
-      <h2>Church Service</h2>
-      <p>The church service is being performed at Church address
-      </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
-      <h2>Crematorium</h2>
-      <p>The service is being held at Crematorium address</p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12:00pm</a>
-      <ul class="fa-ul">
-        <ListItem>Username: <span class="highlight">test123</span> <CopyButton text="test123"></CopyButton></ListItem>
-        <ListItem>Password: <span class="highlight">password123</span> <CopyButton text="password123"></CopyButton></ListItem>
-      </ul>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/rUp6QcRaAAk?controls=0&modestbranding=1&autohide=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 
     </div>
   </div>
