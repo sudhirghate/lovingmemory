2d7be0f40ba697375aa25dd67d297bd74bdabb9a
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 21:14:28 +0530

    Update gallery.vue

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -4,78 +4,16 @@
       <Nav />
     </div>
     <div class="section details">
-      <h1 class="title">Gallery</h1>
-      <CoolLightBox 
-        :items="images" 
-        :index="index"
-        @close="index = null">
-      </CoolLightBox>
+      <h1 class="title">Memories</h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/rUp6QcRaAAk?controls=0&modestbranding=1&autohide=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-      <div class="images-wrapper">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/zSLcrHHX9Yk?controls=0&modestbranding=1&autohide=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import CoolLightBox from 'vue-cool-lightbox'
-import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
- export default {
-  components: {
-    CoolLightBox,
-  },
-
-  data: function () {
-    return {
-      images: [],
-      thumbs: [],
-      index: null
-    };
-  },
-  mounted() {
-    this.importAll(require.context('~/assets/gallery/', true, /\.(png|jpe?g|svg)$/));
-    this.importThumbs(require.context('~/assets/thumbs/', true, /\.(png|jpe?g|svg)$/));
-  },
-
-  methods: {
-    importAll(r) {
-      console.log(r)
-      r.keys().forEach(key => (this.images.push(r(key))));
-    },
-    importThumbs(r) {
-      console.log(r)
-      r.keys().forEach(key => (this.thumbs.push(r(key))));
-    },
-  },
-};
+export default {}
 </script>
 
 <style lang="scss">
-.images-wrapper {
-    margin-bottom: 20px;
-    margin-right: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-    @media screen and (max-width: 800px) {
-      justify-content: center;
-      width: calc(100% + 60px);
-      margin-left: -30px;
-      margin-bottom: 0px;
-    }
-}
-.images-wrapper .image {
-    cursor: pointer;
-    background-position: 50%;
-    background-repeat: no-repeat;
-    background-size: cover;
-    margin: 3px;
-    width: 120px;
-    height: 120px;
-    @media screen and (max-width: 800px) {
-      width: 105px;
-      height: 105px;
-    }
-}
 </style>
