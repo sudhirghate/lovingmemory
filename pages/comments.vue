3fbebafe7bdbee71d525b49dcd7f14036c5fd528
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-06 21:21:23 +0530

    Create comments.vue

---
+++ pages/comments.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v12.0" nonce="7euCH3NC"></script>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section details">
+      <h1 class="title">Service Livestream</h1>
+<div class="fb-comments" data-href="https://sudhir.magnumad.in" data-width="" data-numposts="100"></div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
