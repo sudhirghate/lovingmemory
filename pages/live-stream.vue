ce0c0e8d0acb0a7b730f1ab8ee1ddc730adbaedb
sudhirghate <93827319+sudhirghate@users.noreply.github.com>
2021-11-12 18:12:23 +0530

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -5,8 +5,9 @@
     </div>
     <div class="section details">
       <h1 class="title">Service Livestream</h1>
+      <div class="youtube">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/rUp6QcRaAAk?controls=0&modestbranding=1&autohide=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+      </div>
 
     </div>
   </div>
